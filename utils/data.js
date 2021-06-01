89e6c7de831d4ea8aca7e9d4841634618ac71977
themgi <the.mgi@outlook.com>
2021-06-02 02:10:22 +0500

    dummy data for the products

---
+++ utils/data.js
@@ -0,0 +1,106 @@
+export const PRODUCTS = [
+	{
+		key: 0,
+		title: "HP 24mh FHD Monitor - Computer Monitor with 23.8-Inch IPS Display (1080p) - Built-In Speakers and VESA Mounting - Height/Tilt Adjustment for Ergonomic Viewing - HDMI and DisplayPort - (1D0J9AA#ABA)",
+		description: [
+			{point: "OUTSTANDING VISUALS – This FHD display with IPS technology gives you brilliant visuals and unforgettable quality; with a maximum resolution of 1920 x 1080 at 75 Hz, you’ll experience the image accuracy and wide-viewing spectrums of premium tablets and mobile devices"},
+			{point: "MORE SCREEN, LESS SPACE – Enjoy more desk space than you thought possible with an attractive and ultra-slim design"},
+			{point: "PANORAMIC VIEWING – Vibrant detail from practically any position with consistent color and image clarity maintained across an ultra-wide 178° horizontal and vertical viewing angles"},
+			{point: "MICRO-EDGE DISPLAY – With virtually no bezel encircling the display on three sides, an ultra-wide viewing experience provides for seamless multi-monitor setups"},
+			{point: "EASY CONNECTIVITY – Get the picture quality you’ve been looking for without the additional dongles; easily connect to your PC, gaming console, and peripherals for big-screen entertainment with a broad range of ports, including HDMI, DisplayPort, and VGA ports"},
+			{point: "BUILT-IN SPEAKERS – Experience incredible sound and more immersive entertainment with two built-in 2W speakers"},
+			{point: "LOW BLUE LIGHT – Put less strain on your eyes as a Low Blue Light mode shifts colors to a warmer spectrum and makes whites more natural"}
+		],
+		imageURL: "https://images-na.ssl-images-amazon.com/images/I/91fAU6mxFsL._AC_SX679_.jpg"
+	},
+	{key: 1, title: "SAMSUNG 870 EVO 1TB 2.5 Inch SATA III Internal SSD (MZ-77E1T0B/AM)",
+		description: [
+			{point: "THE SSD ALL-STAR: The latest 870 EVO has indisputable performance, reliability and compatibility built upon Samsung's pioneering technology"},
+			{point: "EXCELLENCE IN PERFORMANCE: Enjoy professional level SSD performance with 870 EVO, which maximizes the SATA interface limit to 560/530 MB/s sequential speeds, accelerates write speeds and maintains long term high performance with a larger variable buffer"},
+			{point: "INDUSTRY DEFINING RELIABILITY: Meet the demands of every task from everyday computing to 8K video processing, with up to 2,400 TBW under a 5 year limited warranty"},
+			{point: "MORE COMPATIBLE THAN EVER: 870 EVO has been compatibility tested for major host systems and applications, including chipsets, motherboards, NAS, and video recording devices"},
+			{point: "UPGRADE WITH EASE: Simply plug 870 EVO into the standard 2.5 inch SATA form factor on your desktop PC or laptop and let the renewed migration software takes care of the rest"},
+			{point: "SAMSUNG MAGICIAN SOFTWARE: Make your drive work smoothly with enhanced Samsung Magician 6 software that helps you easily manage your drive, keep up with the latest updates, monitor the drive's health and status, or even enhance its performance"},
+		],
+		imageURL: "https://images-na.ssl-images-amazon.com/images/I/911ujeCkGfL._AC_SL1500_.jpg"
+	},
+	{
+		key: 2,
+		title: "HP 24mh FHD Monitor - Computer Monitor with 23.8-Inch IPS Display (1080p) - Built-In Speakers and VESA Mounting - Height/Tilt Adjustment for Ergonomic Viewing - HDMI and DisplayPort - (1D0J9AA#ABA)",
+		description: [
+			{point: "OUTSTANDING VISUALS – This FHD display with IPS technology gives you brilliant visuals and unforgettable quality; with a maximum resolution of 1920 x 1080 at 75 Hz, you’ll experience the image accuracy and wide-viewing spectrums of premium tablets and mobile devices"},
+			{point: "MORE SCREEN, LESS SPACE – Enjoy more desk space than you thought possible with an attractive and ultra-slim design"},
+			{point: "PANORAMIC VIEWING – Vibrant detail from practically any position with consistent color and image clarity maintained across an ultra-wide 178° horizontal and vertical viewing angles"},
+			{point: "MICRO-EDGE DISPLAY – With virtually no bezel encircling the display on three sides, an ultra-wide viewing experience provides for seamless multi-monitor setups"},
+			{point: "EASY CONNECTIVITY – Get the picture quality you’ve been looking for without the additional dongles; easily connect to your PC, gaming console, and peripherals for big-screen entertainment with a broad range of ports, including HDMI, DisplayPort, and VGA ports"},
+			{point: "BUILT-IN SPEAKERS – Experience incredible sound and more immersive entertainment with two built-in 2W speakers"},
+			{point: "LOW BLUE LIGHT – Put less strain on your eyes as a Low Blue Light mode shifts colors to a warmer spectrum and makes whites more natural"}
+		],
+		imageURL: "https://images-na.ssl-images-amazon.com/images/I/91fAU6mxFsL._AC_SX679_.jpg"
+	},
+	{key: 3, title: "SAMSUNG 870 EVO 1TB 2.5 Inch SATA III Internal SSD (MZ-77E1T0B/AM)",
+		description: [
+			{point: "THE SSD ALL-STAR: The latest 870 EVO has indisputable performance, reliability and compatibility built upon Samsung's pioneering technology"},
+			{point: "EXCELLENCE IN PERFORMANCE: Enjoy professional level SSD performance with 870 EVO, which maximizes the SATA interface limit to 560/530 MB/s sequential speeds, accelerates write speeds and maintains long term high performance with a larger variable buffer"},
+			{point: "INDUSTRY DEFINING RELIABILITY: Meet the demands of every task from everyday computing to 8K video processing, with up to 2,400 TBW under a 5 year limited warranty"},
+			{point: "MORE COMPATIBLE THAN EVER: 870 EVO has been compatibility tested for major host systems and applications, including chipsets, motherboards, NAS, and video recording devices"},
+			{point: "UPGRADE WITH EASE: Simply plug 870 EVO into the standard 2.5 inch SATA form factor on your desktop PC or laptop and let the renewed migration software takes care of the rest"},
+			{point: "SAMSUNG MAGICIAN SOFTWARE: Make your drive work smoothly with enhanced Samsung Magician 6 software that helps you easily manage your drive, keep up with the latest updates, monitor the drive's health and status, or even enhance its performance"},
+		],
+		imageURL: "https://images-na.ssl-images-amazon.com/images/I/911ujeCkGfL._AC_SL1500_.jpg"
+	},
+	{
+		key: 4,
+		title: "HP 24mh FHD Monitor - Computer Monitor with 23.8-Inch IPS Display (1080p) - Built-In Speakers and VESA Mounting - Height/Tilt Adjustment for Ergonomic Viewing - HDMI and DisplayPort - (1D0J9AA#ABA)",
+		description: [
+			{point: "OUTSTANDING VISUALS – This FHD display with IPS technology gives you brilliant visuals and unforgettable quality; with a maximum resolution of 1920 x 1080 at 75 Hz, you’ll experience the image accuracy and wide-viewing spectrums of premium tablets and mobile devices"},
+			{point: "MORE SCREEN, LESS SPACE – Enjoy more desk space than you thought possible with an attractive and ultra-slim design"},
+			{point: "PANORAMIC VIEWING – Vibrant detail from practically any position with consistent color and image clarity maintained across an ultra-wide 178° horizontal and vertical viewing angles"},
+			{point: "MICRO-EDGE DISPLAY – With virtually no bezel encircling the display on three sides, an ultra-wide viewing experience provides for seamless multi-monitor setups"},
+			{point: "EASY CONNECTIVITY – Get the picture quality you’ve been looking for without the additional dongles; easily connect to your PC, gaming console, and peripherals for big-screen entertainment with a broad range of ports, including HDMI, DisplayPort, and VGA ports"},
+			{point: "BUILT-IN SPEAKERS – Experience incredible sound and more immersive entertainment with two built-in 2W speakers"},
+			{point: "LOW BLUE LIGHT – Put less strain on your eyes as a Low Blue Light mode shifts colors to a warmer spectrum and makes whites more natural"}
+		],
+		imageURL: "https://images-na.ssl-images-amazon.com/images/I/91fAU6mxFsL._AC_SX679_.jpg"
+	},
+	{key: 5, title: "SAMSUNG 870 EVO 1TB 2.5 Inch SATA III Internal SSD (MZ-77E1T0B/AM)",
+		description: [
+			{point: "THE SSD ALL-STAR: The latest 870 EVO has indisputable performance, reliability and compatibility built upon Samsung's pioneering technology"},
+			{point: "EXCELLENCE IN PERFORMANCE: Enjoy professional level SSD performance with 870 EVO, which maximizes the SATA interface limit to 560/530 MB/s sequential speeds, accelerates write speeds and maintains long term high performance with a larger variable buffer"},
+			{point: "INDUSTRY DEFINING RELIABILITY: Meet the demands of every task from everyday computing to 8K video processing, with up to 2,400 TBW under a 5 year limited warranty"},
+			{point: "MORE COMPATIBLE THAN EVER: 870 EVO has been compatibility tested for major host systems and applications, including chipsets, motherboards, NAS, and video recording devices"},
+			{point: "UPGRADE WITH EASE: Simply plug 870 EVO into the standard 2.5 inch SATA form factor on your desktop PC or laptop and let the renewed migration software takes care of the rest"},
+			{point: "SAMSUNG MAGICIAN SOFTWARE: Make your drive work smoothly with enhanced Samsung Magician 6 software that helps you easily manage your drive, keep up with the latest updates, monitor the drive's health and status, or even enhance its performance"},
+		],
+		imageURL: "https://images-na.ssl-images-amazon.com/images/I/911ujeCkGfL._AC_SL1500_.jpg"
+	},
+	{
+		key: 7,
+		title: "HP 24mh FHD Monitor - Computer Monitor with 23.8-Inch IPS Display (1080p) - Built-In Speakers and VESA Mounting - Height/Tilt Adjustment for Ergonomic Viewing - HDMI and DisplayPort - (1D0J9AA#ABA)",
+		description: [
+			{point: "OUTSTANDING VISUALS – This FHD display with IPS technology gives you brilliant visuals and unforgettable quality; with a maximum resolution of 1920 x 1080 at 75 Hz, you’ll experience the image accuracy and wide-viewing spectrums of premium tablets and mobile devices"},
+			{point: "MORE SCREEN, LESS SPACE – Enjoy more desk space than you thought possible with an attractive and ultra-slim design"},
+			{point: "PANORAMIC VIEWING – Vibrant detail from practically any position with consistent color and image clarity maintained across an ultra-wide 178° horizontal and vertical viewing angles"},
+			{point: "MICRO-EDGE DISPLAY – With virtually no bezel encircling the display on three sides, an ultra-wide viewing experience provides for seamless multi-monitor setups"},
+			{point: "EASY CONNECTIVITY – Get the picture quality you’ve been looking for without the additional dongles; easily connect to your PC, gaming console, and peripherals for big-screen entertainment with a broad range of ports, including HDMI, DisplayPort, and VGA ports"},
+			{point: "BUILT-IN SPEAKERS – Experience incredible sound and more immersive entertainment with two built-in 2W speakers"},
+			{point: "LOW BLUE LIGHT – Put less strain on your eyes as a Low Blue Light mode shifts colors to a warmer spectrum and makes whites more natural"}
+		],
+		imageURL: "https://images-na.ssl-images-amazon.com/images/I/91fAU6mxFsL._AC_SX679_.jpg"
+	},
+	{key: 8, title: "SAMSUNG 870 EVO 1TB 2.5 Inch SATA III Internal SSD (MZ-77E1T0B/AM)",
+		description: [
+			{point: "THE SSD ALL-STAR: The latest 870 EVO has indisputable performance, reliability and compatibility built upon Samsung's pioneering technology"},
+			{point: "EXCELLENCE IN PERFORMANCE: Enjoy professional level SSD performance with 870 EVO, which maximizes the SATA interface limit to 560/530 MB/s sequential speeds, accelerates write speeds and maintains long term high performance with a larger variable buffer"},
+			{point: "INDUSTRY DEFINING RELIABILITY: Meet the demands of every task from everyday computing to 8K video processing, with up to 2,400 TBW under a 5 year limited warranty"},
+			{point: "MORE COMPATIBLE THAN EVER: 870 EVO has been compatibility tested for major host systems and applications, including chipsets, motherboards, NAS, and video recording devices"},
+			{point: "UPGRADE WITH EASE: Simply plug 870 EVO into the standard 2.5 inch SATA form factor on your desktop PC or laptop and let the renewed migration software takes care of the rest"},
+			{point: "SAMSUNG MAGICIAN SOFTWARE: Make your drive work smoothly with enhanced Samsung Magician 6 software that helps you easily manage your drive, keep up with the latest updates, monitor the drive's health and status, or even enhance its performance"},
+		],
+		imageURL: "https://images-na.ssl-images-amazon.com/images/I/911ujeCkGfL._AC_SL1500_.jpg"
+	}
+];
+
+const EMPLOYEES = [];
+
+const ORDERS = [];
